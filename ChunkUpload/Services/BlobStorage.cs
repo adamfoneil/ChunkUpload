e4f750f99741d4d23f87d7d7961f1bc47137fdc8
Adam O'Neil <adamosoftware@gmail.com>
2020-08-13 11:47:10 -0400

    blob storage working

---
+++ ChunkUpload/Services/BlobStorage.cs
@@ -0,0 +1,33 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Specialized;
+using ChunkUpload.Interfaces;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ChunkUpload.Services
+{
+    public class BlobStorage : IFileStorage
+    {
+        private readonly string _connectionString;        
+
+        public BlobStorage(string connectionString, string containerName)
+        {
+            _connectionString = connectionString;
+            ContainerName = containerName;
+        }
+
+        public string ContainerName { get; }
+
+        public async Task AppendChunk(Stream content, string name)
+        {
+            var container = new BlobContainerClient(_connectionString, ContainerName);
+            await container.CreateIfNotExistsAsync();
+
+            var appendBlob = new AppendBlobClient(_connectionString, ContainerName, name);
+            await appendBlob.CreateIfNotExistsAsync();
+            await appendBlob.AppendBlockAsync(content);
+
+        }
+    }
+}
--- ChunkUpload/Startup.cs
+++ ChunkUpload/Startup.cs
@@ -20,7 +20,8 @@ namespace ChunkUpload
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<IFileStorage>(new LocalStorage("uploads"));
+            //services.AddSingleton<IFileStorage>(new LocalStorage("uploads"));
+            services.AddSingleton<IFileStorage>(new BlobStorage(Configuration.GetConnectionString("Default"), "chunk-uploads"));
             services.AddRazorPages();
         }
 
