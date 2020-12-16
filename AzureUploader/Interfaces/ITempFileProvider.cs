2e19675b945d98ac49f7784f9a370402e9527cb3
Adam O'Neil <adamosoftware@gmail.com>
2020-12-16 11:39:06 -0500

    added ITempFileProvider

---
+++ AzureUploader/Interfaces/ITempFileProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AzureUploader.Interfaces
+{
+    /// <summary>
+    /// provides a way to avoid using MemoryStream
+    /// </summary>
+    public interface ITempFileProvider
+    {
+        Task ExecuteAsync(byte[] byteArray, Func<Stream, Task> executeWithStream);
+    }
+}
---
+++ AzureUploader/Services/AppDataTempFileProvider.cs
@@ -0,0 +1,58 @@
+﻿using AzureUploader.Interfaces;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AzureUploader.Services
+{
+    public class AppDataTempFileProvider : ITempFileProvider
+    {
+        private readonly string _basePath;
+
+        private static int _fileCount = 0;
+
+        public AppDataTempFileProvider(string basePath, string userName)
+        {
+            _basePath = Path.Combine(basePath, userName, "App_Data");
+        }
+
+        public AppDataTempFileProvider(IWebHostEnvironment environment, string userName) : this(environment.ContentRootPath, userName)
+        {
+        }
+        
+        public async Task ExecuteAsync(byte[] byteArray, Func<Stream, Task> executeWithStream)
+        {
+            if (!Directory.Exists(_basePath)) Directory.CreateDirectory(_basePath);
+
+            // to prevent file name collisions (not sure why this would happen)
+            // we keep incrementing file index as needed
+            string fileName;
+            do
+            {
+                _fileCount++;
+                fileName = Path.Combine(_basePath, $"file{_fileCount}.tmp");
+            } while (File.Exists(fileName));
+
+            try
+            {
+                // first write our byte array to a file, and ensure file is fully written
+                using (var file = File.Create(fileName))
+                {
+                    await file.WriteAsync(byteArray, 0, byteArray.Length);                    
+                }
+
+                // now execute a task that requires stream access to the original byte array (such as a large blob upload)
+                using (var file = File.OpenRead(fileName))
+                {
+                    await executeWithStream(file);
+                }
+            }
+            finally
+            {
+                // even if an exception occurs, our temp file is deleted
+                File.Delete(fileName);
+            }
+        }
+    }
+}
